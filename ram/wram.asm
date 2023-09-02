a299ba3be86995c21f08cce261c595ecaa41ce2a
Lorissa G <lorissabecker@gmail.com>
2023-09-02 10:47:08 -0400

    Added a berry pocket for berries and apricorns

--- ram/wram.asm
+++ ram/wram.asm
@@ -2268,6 +2268,7 @@ wItemsPocketCursor::    db
 wKeyItemsPocketCursor:: db
 wBallsPocketCursor::    db
 wTMHMPocketCursor::     db
+wBerryPocketCursor::	db
 
 wPCItemsScrollPosition::        db
 	ds 1
@@ -2275,6 +2276,7 @@ wItemsPocketScrollPosition::    db
 wKeyItemsPocketScrollPosition:: db
 wBallsPocketScrollPosition::    db
 wTMHMPocketScrollPosition::     db
+wBerryPocketScrollPosition::	db
 
 wSwitchMon::
 wSwitchItem::
@@ -2786,7 +2788,7 @@ NEXTU
 ; catch tutorial dude pack
 wDudeNumItems:: db
 wDudeItems:: ds 2 * 4 + 1
-
+wDudeNumBerries::
 wDudeNumKeyItems:: db
 wDudeKeyItems:: ds 18 + 1
 
@@ -2939,8 +2941,6 @@ endr
 
 wCmdQueue:: ds CMDQUEUE_CAPACITY * CMDQUEUE_ENTRY_SIZE
 
-	ds 40
-
 wMapObjects::
 wPlayerObject:: map_object wPlayer ; player is map object 0
 ; wMap1Object - wMap15Object
@@ -3010,7 +3010,8 @@ wKeyItems:: ds MAX_KEY_ITEMS + 1
 
 wNumBalls:: db
 wBalls:: ds MAX_BALLS * 2 + 1
-
+wNumBerries:: db
+wBerries:: ds MAX_BERRIES * 2 + 1
 wNumPCItems:: db
 wPCItems:: ds MAX_PC_ITEMS * 2 + 1
 
